3a536f3f8aa3f4cdbb9034f6c5f2ce1189a03a6c
exert777 <alter0910@mail.ru>
2024-08-30 10:43:02 +0300

    исправит ошибки линтера

--- src/const.js
+++ src/const.js
@@ -2,8 +2,8 @@ const LoadMessage = {
   EMPTY_LIST: 'Click New Event to create your first point',
   LOADING: 'Loading...',
   FAILED_LOAD: 'Failed to load latest route information'
-}
+};
 
 const SHOW_POINT_COUNT = 3;
 
-export {LoadMessage, SHOW_POINT_COUNT}
+export {LoadMessage, SHOW_POINT_COUNT};
